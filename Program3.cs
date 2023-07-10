3c4fb1dbd2476f76a114b1c134ed842718147687
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-10 07:34:24 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,36 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool isWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Ошибка это не день недели!");
+    return false;
+}
+int weekDay = Prompt("Введите день(цифру) недели:");
+if (ValidateWeekday(weekDay))
+{
+    if (isWeekend(weekDay))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
