a0ef2b4d2a8d186d821d410f8114df87a1c8f60e
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-17 07:10:52 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Ноль запрещен");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите первое число: ");
+int exponent = Prompt("Введите второе число: ");
+if (ValidateExponent (exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
