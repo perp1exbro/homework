2f5e336da358a9d09f9990b0625f547509b4dab6
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-17 07:11:08 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int number = Prompt("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в числе {number} = {SumAllDigit(number)}");
