4fd292c23b1010a121477e2de350739bf402066c
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-24 02:41:14 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
+{
+for (int i = 0; i < m; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+}
+
+int kol(int[] Array)
+{
+int i=0;
+int sum = 0;
+while (i < Array.Length)
+{
+if(Array[i]>0)
+sum = sum + 1;
+i = i + 1;
+}
+return sum;
+}
+
+mas(m);
+Console.Write($"\n Чисел больше нуля: {kol(Array)}");
